de676966c0158dd9345589db4ec368db5da0100b
Liangyu <rilyu@sina.com>
2019-09-19 16:38:41 +0800

    fixed bugs

--- polyfill/ie11.js
+++ polyfill/ie11.js
@@ -1,5 +1,10 @@
 // ie11.js
 
+// https://babeljs.io/docs/en/babel-polyfill
+// core-js/stable not working (why?), must add next line to the top of your entry file.
+// require('core-js/stable');
+require('regenerator-runtime/runtime');
+
 if (typeof Promise === 'undefined') {
   require('promise/lib/rejection-tracking').enable();
   if (typeof window !== 'undefined') {
--- polyfill/stable.js
+++
@@ -1,5 +0,0 @@
-// stable.js
-// https://babeljs.io/docs/en/babel-polyfill
-
-require('core-js/stable');
-require('regenerator-runtime/runtime');
